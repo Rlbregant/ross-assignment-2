85fee4743ecdfba46108562fb0685e67651b1c6b
Ross <23426097+Rlbregant@users.noreply.github.com>
2022-09-26 08:50:59 -0700

    Final version of the Guessing Game

--- src/main/java/com/coderscampus/myapp/main/App.java
+++ src/main/java/com/coderscampus/myapp/main/App.java
@@ -6,57 +6,57 @@ import java.util.Scanner;
 import com.coderscampus.myapp.service.MyService;
 
 public class App {
-	
-	private MyService myService = new MyService();
-	
+
 	public static void main(String[] args) {
 		new App().execute();
 	}
 
 	private void execute() {
-		
-		System.out.println("Welcome to the Guessing Game!");
-		
-		System.out.println("Pick a number between 1 and 100.");
-		
-		Random random = new Random();
-		random.nextInt(101);
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		int userInput = scanner.nextInt();
-		
-		
-		System.out.println(“You lose, the number to guess was " + random.nextInt(101));
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		scanner.close();
-		// Your code goes here, not in the main() method
-		// This removes any need to consume statics
-		// The code below is to be deleted once you understand what it does
-		System.out.println(AppConstants.EXAMPLE_CONSTANT);
-		System.out.println(" 2 * 17 = " + myService.multiply(2, 17));
-		System.out.println("Contents of the file at " + AppConstants.EXAMPLE_FILE_PATH);
-		System.out.println(myService.fileContents(AppConstants.EXAMPLE_FILE_PATH));
-	}
 
+		System.out.println("Welcome to the Guessing Game!");
+
+		System.out.println("Pick a number between 1 and 100.");
+
+		Random random = new Random();
+		int randomNumber = random.nextInt(101);
+
+		Scanner scanner = new Scanner(System.in);
+		int totalGuesses = 5;
+
+		while (totalGuesses >= 0) {
+
+			int userInput = scanner.nextInt();
+
+			if (userInput < 0 || userInput > 100) {
+
+				System.out.println("Your guess is not between 1 and 100, please try again.");
+
+			} else if (totalGuesses == 1) {
+
+				System.out.println("You lose, the number to guess was " + randomNumber + ".");
+				break;
+
+			} else if (userInput > randomNumber) {
+
+				System.out.println("Please pick a lower number.");
+				totalGuesses--;
+				System.out.println("You have " + totalGuesses + " guesses remaining.");
+
+			}
+
+			else if (userInput < randomNumber) {
+				System.out.println("Please pick a higher number.");
+				totalGuesses--;
+				System.out.println("You have " + totalGuesses + " guesses remaining.");
+
+			}
+
+			else {
+
+				System.out.println("You Win!");
+				scanner.close();
+			}
+
+		}
+	}
 }
